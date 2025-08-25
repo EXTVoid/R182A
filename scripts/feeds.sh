2abd00b2aa2e3b9b078660e3e36db4f3ffa5f5e5
EXTVoid <175212606+EXTVoid@users.noreply.github.com>
2025-08-25 08:37:04 +0800

    Update feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -7,12 +7,12 @@ src-git small_package https://github.com/kenzok8/small-package
 # src-git small https://github.com/kenzok8/small
 
 # Extra Packages
-src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite-js
+src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
 src-git lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
 src-git my_pkg https://github.com/EXTVoid/PKG
 src-git modemband https://github.com/4IceG/luci-app-modemband
 src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
-src-git sms_tool https://github.com/4IceG/luci-app-sms-tool
+src-git sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
 src-git xray https://github.com/yichya/luci-app-xray
 src-git v2raya https://github.com/v2rayA/v2raya-openwrt
 src-git zapret https://github.com/remittor/zapret-openwrt
