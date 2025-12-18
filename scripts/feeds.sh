d6dccee9114e4d2010e3eb137e169b7709e6a4bd
EXTVoid <175212606+EXTVoid@users.noreply.github.com>
2025-12-18 23:24:39 +0800

    Fix src-git lines for v2raya and zapret

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -14,8 +14,8 @@ src-git modemband https://github.com/4IceG/luci-app-modemband
 src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
 src-git sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
 src-git xray https://github.com/yichya/luci-app-xray
-src-git v2raya https://github.com/v2rayA/v2raya-openwrt;zap1
-src-git zapret https://github.com/remittor/zapret-openwrt
+src-git v2raya https://github.com/v2rayA/v2raya-openwrt
+src-git zapret https://github.com/remittor/zapret-openwrt;zap1
 EOF
 
 exit 0
