fc2c7692eefe8e9b63ae12f5af55c00822744e7c
EXTVoid <175212606+EXTVoid@users.noreply.github.com>
2026-01-14 11:35:18 +0800

    Modify feeds.sh to update and add feeds
    Update OpenWrt feeds configuration and add extra feeds.

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Updating OpenWrt feeds configuration..."
+sed -i 's|git\.openwrt\.org/\(feed\|project\)|github\.com/openwrt|g' ./feeds.conf.default
+
 cat >> ./feeds.conf.default <<EOF
 # Extra Feeds
 src-git small_package https://github.com/kenzok8/small-package
