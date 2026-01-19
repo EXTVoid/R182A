5bfe159e2667ce942eed9872b7ac32b7d969166e
EXTVoid <175212606+EXTVoid@users.noreply.github.com>
2026-01-20 07:07:45 +0800

    Update zapret source URL in feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -18,7 +18,7 @@ src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
 src-git sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
 src-git xray https://github.com/yichya/luci-app-xray
 src-git v2raya https://github.com/v2rayA/v2raya-openwrt
-src-git zapret https://github.com/remittor/zapret-openwrt;zap1
+src-git zapret https://github.com/remittor/zapret-openwrt
 EOF
 
 exit 0
